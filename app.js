dce4aaaffa98bb6e69b3e4bc10f39e62119a1edf
zhilinIgor <zhilin.igor@gmail.com>
2020-10-11 07:09:17 -0400

    mongoose added

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+
+mongoose.connect('mongodb+srv://IZhilin:' + process.env.MONGO_ATLAS_PW + '@cluster0.ujhe9.mongodb.net/<dbname>?retryWrites=true&w=majority\n',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
